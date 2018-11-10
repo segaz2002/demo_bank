778a9bdbdc5afa685634007de927e9db479a6a6e
Gabriel <segaz2002@gmail.com>
2018-11-10 17:12:43 +0200

    refactor

--- index.js
+++ index.js
@@ -11,32 +11,6 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-readBody = (req, res, next)=>{
-  let body = '';
-  req.on('data', chunk => {
-    body += chunk.toString(); // convert Buffer to string
-  });
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    req.payload = data
-    next()
-    res.end('ok');
-  });
-  
-}
-
-function genAccountNumber() {
-  const accountNumberLength = 10;
-  const possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  var accNumber = "";
-  //Loop for repetition
-  for (let i = 0; i <= accountNumberLength; i++){
-    const rand = Math.floor(Math.random() * possibleChars.length)
-    accNumber= accNumber + possibleChars.charAt(rand)
-  }
-  return accNumber;
-}
-
 app.post('/accounts/new', readBody, function (req, res) {
   userInfo  = req.payload
   userInfo.accountNumber = "REALOAD" + genAccountNumber();
@@ -57,3 +31,29 @@ app.post('/accounts/:accnumber/balance', readBody, function (req, res) {
 app.listen(3000, ()=>{
   console.log("Application is listening on port 3000")
 })
+
+function readBody(req, res, next) {
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString(); // convert Buffer to string
+  });
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    req.payload = data
+    next()
+    res.end('ok');
+  });
+
+}
+
+function genAccountNumber() {
+  const accountNumberLength = 15;
+  const possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  var accNumber = "";
+  //Loop for repetition
+  for (let i = 0; i <= accountNumberLength; i++) {
+    const rand = Math.floor(Math.random() * possibleChars.length)
+    accNumber = accNumber + possibleChars.charAt(rand)
+  }
+  return accNumber;
+}
